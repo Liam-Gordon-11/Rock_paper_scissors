99a3547239e763277a16bbd12fd48947759ba643
Liam Gordon <gordonliam33@gmail.com>
2023-05-31 21:51:42 +0200

    switch -> nested ifs| break; was breaking loops so not good | v2

--- rps.js
+++ rps.js
@@ -11,19 +11,18 @@ for (let i = 0; i < 5; i++){
 
 user_input = prompt("Play").toUpperCase();
 rand_int = Math.floor(Math.random()*3);
-switch(user_input){
-    case "ROCK": user = 0;break;
-    case "SCISSORS": user = 1;break;
-    case "PAPER": user = 2;break;
-    default : 
-        i --;
 
+    if (input_user =="ROCK"){ user = 0;}
+    else if (input_user =="SCISSORS"){ user = 1;}
+    else if (input_user == "PAPER"){ user = 2;}
+    else{
+        i --;
+    }
 if (user == rand_int){
     console.log("Draw");
 }else if ((user == 0 && rand_int == 1) || (user == 1 && rand_int == 2) || (user == 2 && rand_int == 0)){
     console.log("You Win!!");
 }else{
     console.log("You lose!!");
-}
 }
 }
