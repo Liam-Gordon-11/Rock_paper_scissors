4254edb6599fce86e60083c4c84b617edcd2ada7
Liam Gordon <gordonliam33@gmail.com>
2023-06-10 00:45:41 +0200

    done

--- rps.js
+++ rps.js
@@ -1,28 +1,49 @@
 
-let user_input = "";
-let  rand_int = 0;
-let user = 0;
 
 // rock : 0
 // scissors : 1
 // paper : 2
+let score = [0,0];
 
-for (let i = 0; i < 5; i++){
-
-input_user = prompt("Play").toUpperCase();
-rand_int = Math.floor(Math.random()*3);
-
-    if (input_user =="ROCK"){ user = 0;}
-    else if (input_user =="SCISSORS"){ user = 1;}
-    else if (input_user == "PAPER"){ user = 2;}
-    else{
-        i --;
+let arrImages = ["images/rock.png","images/scissors.jpg","images/paper.png"];
+function changeImg(user,imgId){
+    if (user == 0){
+        document.getElementById(imgId).src="images/rock.png";
+        PlayRound(0);
+    }else if(user == 1){
+        document.getElementById(imgId).src="images/scissors.jpg";
+        PlayRound(1);
+    }else{
+        document.getElementById(imgId).src="images/paper.png";
+        PlayRound(2);
     }
-if (user == rand_int){
-    console.log("Draw");
-}else if ((user == 0 && rand_int == 1) || (user == 1 && rand_int == 2) || (user == 2 && rand_int == 0)){
-    console.log("You Win!!");
-}else{
-    console.log("You lose!!");
 }
+
+function PlayRound(user){
+    let  rand_int = 0;
+    const imageElement = document.getElementById("computerImg"); // Get the image element
+    const playerBorder = document.getElementById("playerSqr");
+    const compBorder = document.getElementById("compSqr");
+    const display = document.getElementById("score");
+
+    rand_int = Math.floor(Math.random()*3);
+    imageElement.src = arrImages[rand_int]; 
+    if (user == rand_int){
+        console.log("Draw");
+        compBorder.style.border="10px solid orange";
+        playerBorder.style.border="10px solid orange";
+    }else if ((user == 0 && rand_int == 1) || (user == 1 && rand_int == 2) || (user == 2 && rand_int == 0)){
+        console.log("You Win!!");
+        score[1] = score[1] + 1;
+        compBorder.style.border="10px solid red";
+        playerBorder.style.border="10px solid green";
+    }else{
+        console.log("You lose!!");
+        score[0] = score[0]+1;
+        compBorder.style.border="10px solid green";
+        playerBorder.style.border="10px solid red";
+    }
+
+    display.innerHTML= `${score[0]} : ${score[1]}`;
 }
+
